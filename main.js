0945a0e920189e8503419ccbce196a4ebec7c7bc
PaoloNapoli97 <paolonapoli@live.it>
2022-11-10 16:27:24 +0100

    Fix milestone 2

--- main.js
+++ main.js
@@ -23,43 +23,92 @@ const images = [
         text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
     }
 ];
+
+// FUNCTION
+
+function changeSlide(direction){
+
+    itemslist[active].classList.remove("item-active");
+    if(direction === 'next'){
+        if(active === images.length -1){
+            active = 0;
+        }
+        else{
+            active++;
+        }
+    }
+    else if( direction === 'prev') {
+        if(active === 0){
+            active = images.length -1;
+        }
+        else{
+            active--;
+        }
+    }
+    itemslist[active].classList.add("item-active");
+}
+
+// 
+
 let active = 0;
 const carousel = document.querySelector('.items');
+const carouselGallery = document.querySelector('.image-gallery');
 
+
+// MAIN
+
+// Metodo col classico ciclo for
 
 for ( let i = 0; i < images.length; i++){
     const element = images[i];
 
+    const templateGallery = document.getElementById('gallery-template').content.cloneNode(true);
     const template = document.getElementById('carousel-template').content.cloneNode(true);
+    if ( i === active) {
+        template.querySelector('.item').classList.add('item-active')
+    }
 
     template.querySelector('.game-image').src = element.image;
+    templateGallery.querySelector('.gallery-image').src = element.image;
 
     template.querySelector('.game-title').innerHTML = element.title;
     template.querySelector('.game-description').innerHTML = element.text;
 
+    carouselGallery.append(templateGallery);
     carousel.append(template);
 };
 
-document.querySelector('.next').addEventListener("click" , function(){
-    const itemslist = document.querySelectorAll(".item");
-    itemslist[active].classList.remove("item-active");
-    if(active === images.length -1){
-        active = 0;
-    }
-    else{
-        active++;
-    }
-    itemslist[active].classList.add("item-active");
+// Ciclo forEach fatto in classe e modificato
+
+// images.forEach((elm, index) => {
+//     // Clono il template delle thumb
+//     const templateGallery = document.getElementById('gallery-template').content.cloneNode(true);
+//     const template = document.getElementById('carousel-template').content.cloneNode(true);
+//     // Compilo l'html del template appena clonato
+//     // SE l'elemento su cui sto ciclando ha l'indice === currentImageIndex gli applica la classe active
+//     if( index === active ) {
+//         template.querySelector('.item').classList.add('item-active');
+//     }
+//     templateGallery.querySelector('img').src = elm.image;
+//     templateGallery.querySelector('img').alt = elm.title;
+
+//     template.querySelector('img').src = elm.image;
+
+//     template.querySelector('.game-title').innerHTML = elm.title;
+//     template.querySelector('.game-description').innerHTML = elm.text;
+//     // Appendo il template nel thumbs container
+//     carouselGallery.append(templateGallery);
+//     carousel.append(template);
+// });
+
+const itemslist = document.querySelectorAll(".item");
+
+const prev = document.querySelector('.prev');
+prev.addEventListener("click" , function(){
+    changeSlide('prev');
 })
 
-document.querySelector('.prev').addEventListener("click" , function(){
-    const itemslist = document.querySelectorAll(".item");
-    itemslist[active].classList.remove("item-active");
-    if(active === 0){
-        active = images.length -1;
-    }
-    else{
-        active--;
-    }
-    itemslist[active].classList.add("item-active");
+const next = document.querySelector('.next');
+next.addEventListener("click" , function(){
+    changeSlide('next');
 })
